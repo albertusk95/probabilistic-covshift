ce388e225c2e1928f8cbe3183f17d8b5c09d492e
Albertus Kelvin <albertus.kelvin@gdplabs.id>
2020-03-30 14:39:24 +0700

    update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from setuptools import setup, find_packages
 
+setuptools_kwargs = {
+    "install_requires": [
+        # H2O dependencies
+        "requests == 2.22.0",
+        "tabulate == 0.8.6",
+        "colorama == 0.4.3",
+        "future == 0.18.2",
+        # h2o python driver
+        "h2o == 3.28.0.2",
+        # pyspark
+        "pyspark == 2.4.0"
+    ],
+    "zip_safe": False,
+}
 
 setup(name='probabilistic-covshift',
       version='0.1',
       description='Probabilistic Classification for Density Ratio Estimation',
       url='https://github.com/albertusk95/probabilistic-covshift',
-      author='albertus.kelvin',
+      author='Albertus Kelvin',
       license='Copyright (C) Albertus Kelvin - All Rights Reserved',
       python_requires=">=3.7.0",
-      packages=find_packages())
+      packages=find_packages(),
+      **setuptools_kwargs)
