2a0b1f5c2e4e97a9d09e9d11921ca4277ade8038
Albertus Kelvin <albertus.kelvin@gdplabs.id>
2020-03-30 14:28:30 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='probabilistic-covshift',
+      version='0.1',
+      description='Probabilistic Classification for Density Ratio Estimation',
+      url='https://github.com/albertusk95/probabilistic-covshift',
+      author='albertus.kelvin',
+      license='Copyright (C) Albertus Kelvin - All Rights Reserved',
+      python_requires=">=3.7.0",
+      packages=find_packages())
